3dd8a59dcdac9ecbf329e15460d5e09e13b442f6
Brett H. Farris <bretthfarris@gmmmail.com>
2025-07-20 00:55:14 -0500

    moved vitest.config.ts to src directory

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    include: ['src/tests/**/*.test.ts'],
+    include: ['./tests/**/*.test.ts'],
   },
 });
